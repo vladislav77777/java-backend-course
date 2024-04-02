f2a8efeb89973916df813bb93c633c9e3af31e02
vladislav77777 <vladoss7700@mail.ru>
2024-04-02 16:47:55 +0300

    Add kafka configuration

--- scrapper/src/main/java/edu/java/configuration/ApplicationConfig.java
+++ scrapper/src/main/java/edu/java/configuration/ApplicationConfig.java
@@ -14,7 +14,9 @@ public record ApplicationConfig(
     LinkClient client,
     DebugInformation debugInformation,
     AccessType databaseAccessType,
-    Retry retry
+    Retry retry,
+    KafkaConfigInfo kafkaConfigInfo,
+    Boolean useQueue
 
 ) {
     public record Scheduler(
@@ -66,6 +68,18 @@ public record ApplicationConfig(
                 Long maxIntervalMillis
             ) {
             }
+        }
+    }
+
+    public record KafkaConfigInfo(
+        String bootstrapServers,
+        UpdatesTopic updatesTopic
+    ) {
+        public record UpdatesTopic(
+            String name,
+            Integer partitions,
+            Integer replicas
+        ) {
         }
     }
 }
